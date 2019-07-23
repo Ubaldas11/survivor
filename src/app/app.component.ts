c3691cd095172d59080c85208571c1a9d3907f1d
Simas Kizys <Simas.Kizys@devbridge.com>
2019-07-23 13:56:08 +0300

    hide header for unauthenticated users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from './services/auth.service';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'survivor';
+export class AppComponent implements OnInit, OnDestroy {
+  public title = 'survivor';
+  public isAuthenticated: boolean;
+  private destroy: Subject<void> = new Subject<void>();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+    this.authService.authStatusChanged.pipe(
+      takeUntil(this.destroy)
+    ).subscribe((isAuthenticated: boolean) => {
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.unsubscribe();
+  }
 }
