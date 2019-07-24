0faeb6c0130749449bb2e307d8bb2df3a052d8ea
Simas Kizys <Simas.Kizys@devbridge.com>
2019-07-24 14:17:27 +0300

    add firebase functions

---
+++ survivor-functions/functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+const answer1 = 'test1';
+const answer2 = 'test2';
+const answer3 = 'test3';
+const app = express();
+app.use(cors({ origin: true }));
+app.post('/', (req, res) => {
+    if(
+        req.body.answer1 == answer1
+        && req.body.answer2 == answer2
+        && req.body.answer3 == answer3
+    ) {
+        res.send("Stai tavo hintas.")
+    } else {
+        res.send("Kask giliau.")
+    }
+});
+
+exports.hints = functions.https.onRequest(app);
